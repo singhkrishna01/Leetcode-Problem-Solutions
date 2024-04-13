eac732fcf026587fcc00d74296d2f0ec0a0dc301
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-04-13 19:52:17 +0530

    Time: 32 ms (59.51%), Space: 17.5 MB (67.14%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,41 +1,133 @@
-#include <vector>
-#include <stack>
-#include <algorithm>
+// class Solution {
+// public:
+//     // int largestRectangleArea(vector<int>& heights) {
+//     //     int n = heights.size();
+//     //   stack < int > st;
+//     //   int leftsmall[n], rightsmall[n];
+//     //   for (int i = 0; i < n; i++) {
+//     //     while (!st.empty() && heights[st.top()] >= heights[i]) {
+//     //       st.pop();
+//     //     }
+//     //     if (st.empty())
+//     //       leftsmall[i] = 0;
+//     //     else
+//     //       leftsmall[i] = st.top() + 1;
+//     //     st.push(i);
+//     //   }
+//     //   // clear the stack to be re-used
+//     //   while (!st.empty())
+//     //     st.pop();
 
-using namespace std;
+//     //   for (int i = n - 1; i >= 0; i--) {
+//     //     while (!st.empty() && heights[st.top()] >= heights[i])
+//     //       st.pop();
+
+//     //     if (st.empty())
+//     //       rightsmall[i] = n - 1;
+//     //     else
+//     //       rightsmall[i] = st.top() - 1;
+
+//     //     st.push(i);
+//     //   }
+//     //   int maxA = 0;
+//     //   for (int i = 0; i < n; i++) {
+//     //     maxA = max(maxA, heights[i] * (rightsmall[i] - leftsmall[i] + 1));
+//     //   }
+//     //   return maxA;
+//     // }
+
+
+
+
+
+// int largestRectangleArea(vector < int > & histo) {
+// 	stack < int > st;
+// 	int maxA = 0;
+// 	int n = histo.size();
+// 	for (int i = 0; i <= n; i++) {
+// 		while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+// 			int height = histo[st.top()];
+// 			st.pop();
+// 			int width;
+// 			if (st.empty())
+// 				width = i;
+// 			else
+// 				width = i - st.top() - 1;
+// 			maxA = max(maxA, width * height);
+// 		}
+// 		st.push(i);
+// 	}
+// 	return maxA;
+// }
+
+
+//     int maximalRectangle(vector<vector<char>>& matrix) {
+//         int n= matrix.size();
+//         int m=matrix[0].size();
+//            vector<int>height(m,0);
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(matrix[i][j]=='1') height[j]++;
+//                 else height[j]=0;
+//             }
+//         }
+
+//         int maxArea=largestRectangleArea(height);
+
+//         int ans=0;
+
+//         ans= max(ans,maxArea);
+
+//         return ans;
+
+//     }
+// };
+
+
 
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int ans = 0;
-        stack<int> stk;
-        
-        for (int i = 0; i <= heights.size(); ++i) {
-            while (!stk.empty() && (i == heights.size() || heights[stk.top()] > heights[i])) {
-                int h = heights[stk.top()];
-                stk.pop();
-                int w = stk.empty() ? i : i - stk.top() - 1;
-                ans = max(ans, h * w);
-            }
-            stk.push(i);
-        }
-        
-        return ans;
-    }
-    
+
+int largestRectangleArea(vector < int > & histo) {
+	stack < int > st;
+	int maxA = 0;
+	int n = histo.size();
+	for (int i = 0; i <= n; i++) {
+		while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+			int height = histo[st.top()];
+			st.pop();
+			int width;
+			if (st.empty())
+				width = i;
+			else
+				width = i - st.top() - 1;
+			maxA = max(maxA, width * height);
+		}
+		st.push(i);
+	}
+	return maxA;
+}
+
+int solve(vector<vector<char>>&mat, int n, int m) {
+	int maxArea = 0;
+	vector<int> height(m, 0);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (mat[i][j] == '1') height[j]++;
+			else height[j] = 0;
+		}
+		int area = largestRectangleArea(height);
+		maxArea = max(area, maxArea);
+	}
+
+	return maxArea;
+}
+
+
     int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty())
-            return 0;
-        
-        int ans = 0;
-        vector<int> hist(matrix[0].size(), 0);
-        
-        for (auto & row : matrix) {
-            for (int i = 0; i < row.size(); ++i)
-                hist[i] = row[i] == '0' ? 0 : hist[i] + 1;
-            ans = max(ans, largestRectangleArea(hist));
-        }
-        
-        return ans;
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        return solve(matrix, n, m);
     }
 };
