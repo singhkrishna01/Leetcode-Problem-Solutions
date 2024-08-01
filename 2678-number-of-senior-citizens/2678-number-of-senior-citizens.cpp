2ea6f7a187e556ad2229a164db095e3d5c7bf455
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-01 09:28:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for (auto& x : details) {
+            int age = stoi(x.substr(11, 2));
+            
+        }
+        return ans;
+    }
+};
