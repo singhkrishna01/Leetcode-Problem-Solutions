3f160694371beb556cd3a719d20a8516fd7acc8e
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-02 16:18:06 +0530

    Time: 6 ms (16.95%), Space: 9.5 MB (68.82%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        vector<int>ans;
+        int n1=arr1.size();
+        int n2=arr2.size();
+        int i=0,j=0;
+        while(j<n2){
+            for(int i=0;i<n1;i++){
+                if(arr1[i]==arr2[j]){
+                    ans.push_back(arr1[i]);
+                    arr1[i]=-1;
+                }
+            }
+            j++;
+        }
+        int size=ans.size();
+        for(int i=0;i<n1;i++){
+            if(arr1[i]>-1){
+                ans.push_back(arr1[i]);
+            }
+        }
+        sort(ans.begin()+size,ans.end());
+        return ans;
+
+    }
+};
