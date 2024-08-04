9b1956ceb08d4297e500a4e2f140f4ccf8be13ec
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-04 12:15:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        int arr[n * (n + 1) / 2];
+        for (int i = 0, k = 0; i < n; ++i) {
+            int s = 0;
+            for (int j = i; j < n; ++j) {
+                s += nums[j];
+                arr[k++] = s;
+            }
+        }
+        sort(arr, arr + n * (n + 1) / 2);
+        int ans = 0;
+        
+        }
+        return ans;
+    }
+};
