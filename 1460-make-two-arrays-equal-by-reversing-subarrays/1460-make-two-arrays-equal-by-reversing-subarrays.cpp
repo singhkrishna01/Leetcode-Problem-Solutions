c48ff656cc6b1b898996eb0870afd81e624e953f
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-04 12:13:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        vector<int> cnt1(1001);
+        vector<int> cnt2(1001);
+        for (int& v : target) {
+            ++cnt1[v];
+        }
+        
+        }
+        
+    }
+};
