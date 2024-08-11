0ab9bdec82d5f63dc59a72e510cc3163cd5d8c57
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-11 23:41:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1568-minimum-number-of-days-to-disconnect-island/1568-minimum-number-of-days-to-disconnect-island.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minDays(vector<vector<int>>& grid) {
+        auto count_islands = [&]() -> int {
+            vector<vector<int>> seen(grid.size(), vector<int>(grid[0].size(), 0));
+            int islands = 0;
+            
+                    int nr = r + dr, nc = c + dc;
+                    if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && grid[nr][nc] == 1 && !seen[nr][nc]) {
+                        dfs(nr, nc);
+                    }
+                }
+            };
+            
+            for (int i = 0; i < grid.size(); i++) {
+                for (int j = 0; j < grid[0].size(); j++) {
+                    if (grid[i][j] == 1 && !seen[i][j]) {
+                        islands++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            return islands;
+        };
+        
+        if (count_islands() != 1) return 0;
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;
+                    if (count_islands() != 1) return 1;
+                    grid[i][j] = 1;
+                }
+            }
+        }
+        
+        return 2;
+    }
+};
