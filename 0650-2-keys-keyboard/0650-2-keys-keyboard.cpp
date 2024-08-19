ac7f4d4e8853a85095da0fb0d3692eeaa36d6fa6
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-08-19 20:45:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        if (n == 1) return 0;
+        
+        int steps = 0;
+        int factor = 2;
+        
+        
+            }
+            factor++;
+        }
+        
+        return steps;
+    }
+};
