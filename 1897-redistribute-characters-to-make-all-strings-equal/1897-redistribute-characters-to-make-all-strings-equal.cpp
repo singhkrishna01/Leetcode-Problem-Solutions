9fc37c0a5d15b6ea1a2610f6825e5a9d6c856b77
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-04 20:23:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // Function to check if the characters in the words can be rearranged
+    // to make each word have the same characters in the same frequency.
+    bool makeEqual(vector<string>& words) {
+
+        // Array to store the count of each character (initialized to 0).
+        vector<int> characterCount(26, 0);
+
+        // Loop through each word in the input vector 'words'.
+        for (const string& inputWord : words) {
+            // Loop through each character in the current word.
+            for (char inputChar : inputWord) {
+                // Increment the count of the current character in 'characterCount'.
+                characterCount[inputChar - 'a']++;
+            }
+        }
+
+        // Check if the count of each character is divisible by the total number of words.
+        
+                // If not divisible, characters cannot be rearranged to make all words equal.
+                return false;
+            }
+        }
+
+        // If all characters can be rearranged to make words equal, return true.
+        return true;
+    }
+};
