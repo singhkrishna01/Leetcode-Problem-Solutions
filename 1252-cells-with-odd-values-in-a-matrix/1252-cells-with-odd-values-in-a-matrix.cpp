45c36bd52f595e4f5584faac535c7b569344af87
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-08 01:57:07 +0530

    Time: 7 ms (12.08%), Space: 10.5 MB (32.5%) - LeetHub

---
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int oddCells(int m, int n, vector<vector<int>>& indices) {
+        vector<vector<int>> matrix (m, vector<int>(n, 0));
+
+        for (int i = 0; i < indices.size(); i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[j][indices[i][1]]++;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                matrix[indices[i][0]][j]++;
+            }
+        }
+
+        int odds = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] % 2 != 0) odds++;
+            }
+        }
+
+        return odds;
+    }
+};
