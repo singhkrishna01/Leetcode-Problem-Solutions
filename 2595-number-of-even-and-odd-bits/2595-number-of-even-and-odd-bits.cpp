f0834094729cf8ca2b022936e14f68c61488b736
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-09 14:19:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> evenOddBit(int n) {
+
+        int even = 0 , odd = 0 , count = 1;
+                    even++;
+                else
+                    odd++;
+            }
+        }
+
+        return {even,odd};
+    }
+};
