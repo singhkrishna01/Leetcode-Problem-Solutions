bcfbd26d7301ad098967eddef0e0ff9ad25c01a7
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-12 20:59:15 +0530

    Time: 3 ms (98.24%), Space: 29.4 MB (61.67%) - LeetHub

--- 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.cpp
+++ 2917-find-the-k-or-of-an-array/2917-find-the-k-or-of-an-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> bit_count(32, 0);
         int result = 0;
         for (const int& num : nums) {
-            {
+            for (int i = 0; i < 32; i++) {
                 if (((1 << i) & num) == 0) continue;
                 if ((++bit_count[i]) == k) result |= (1 << i);
             }
