2f9bdd60dacc31c6a2d551ec5fc358711c311560
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-14 13:59:38 +0530

    Time: 53 ms (94.96%), Space: 85.5 MB (11.62%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -5,6 +5,9 @@ public:
         int m=max_element(nums.begin(), nums.end())-nums.begin();
         int maxLen=0, x = nums[m];
 
+        for (int i = m; i < n; i++) {
+            int len = 0;
+
             while (i < n && nums[i] == x) {
                 i++;
                 len++;
