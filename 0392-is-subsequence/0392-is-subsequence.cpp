450abf29466b5d1699dfd190d4735c8c0b0e5221
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-09-21 19:15:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < t.size()) {
+                i++;
+                j++;
+                    return true;
+                }
+            }
+             else {
+                i++;
+            }
+        }
+        return j == s.size();
+    }
+};
