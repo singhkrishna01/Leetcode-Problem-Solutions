143e541ef61f12a12ebcac6532fe5b9b7a00007c
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-10-01 21:17:07 +0530

    Time: 42 ms (98.81%), Space: 64.5 MB (76.85%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -3,13 +3,14 @@ public:
     bool canArrange(vector<int>& arr, int k) {
         vector<int> freq(k,0);
         for(auto& it: arr){
+            int rem = it % k;
             if(rem < 0) rem += k;
             freq[rem]++;
         }
 
         if(freq[0] % 2) return false;
 
-        for(int i=1; i<=k/2; i--){
+        for(int i=1; i<=k/2; i++){
             if(freq[i] != freq[k-i]) return false;
         }
         return true;
