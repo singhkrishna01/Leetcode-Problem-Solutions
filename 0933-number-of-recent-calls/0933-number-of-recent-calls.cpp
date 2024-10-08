d82448edde71294740006d6d96862220b640d828
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-10-08 23:58:30 +0530

    Time: 119 ms (38.48%), Space: 63.2 MB (16.02%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -1,53 +1,18 @@
-// Method 1 - using map - bad TC
-// class RecentCounter {
-// public:
-//     unordered_map<int,int> m;
-//     RecentCounter() {
-        
-//     }
-    
-//     int ping(int t) {
-//         m[t]++;
+#include <vector>
 
-//         int ans = 0;
-
-//         int lower = t - 3000, upper = t;
-//         for(auto &x : m){
-//             if(x.first >= lower && x.second <= upper){
-//                 ans = ans + x.second;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-// Method 2 - using deque - best Time complexity
 class RecentCounter {
-public:
-    queue<int> q;
-    RecentCounter() {
-        
-    }
-    
-    int ping(int t) {
-        // we should keep only t-3000 time elements in queue
-        int lower = t - 3000;
-        // pop all unecessary items
-        while(!q.empty() && lower > q.front()){
-            q.pop();
-        }
+private:
+    std::vector<int> records;
+    int start;
 
-        q.push(t);
-        // current size of queue
-        return q.size();
+public:
+    RecentCounter() : start(0) {}
+
+    int ping(int t) {
+        records.push_back(t);
+        while (records[start] < t - 3000) {
+            start++;
+        }
+        return records.size() - start;
     }
 };
-
-
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter* obj = new RecentCounter();
- * int param_1 = obj->ping(t);
- */
