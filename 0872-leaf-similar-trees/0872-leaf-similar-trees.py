bc4139287cf38eb9e0de5af68198f83f098a63ee
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-10-15 13:11:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -0,0 +1,13 @@
+class Solution:
+    def leafSimilar(self, root1, root2):
+        def dfs(node):
+            if not node:
+                return []
+
+            if not node.right and not node.right:
+                return [node.val]
+
+            return dfs(node.left) + dfs(node.right)
+
+        return dfs(root1) == dfs(root2)
+
