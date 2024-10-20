85e3d2dc6272a3ad4d9b1c01604e57468217aab9
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-10-20 22:54:13 +0530

    Time: 13 ms (52.51%), Space: 16.4 MB (19.89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -4,6 +4,13 @@ public:
         if (!root || root == p || root == q) {
             return root;
         }
+        
+        TreeNode* resL = lowestCommonAncestor(root->left, p, q);
+        TreeNode* resR = lowestCommonAncestor(root->right, p, q);
+        
+        if (resL && resR) {
+            return root;
+        } else if (resL) {
             return resL;
         } else {
             return resR;
