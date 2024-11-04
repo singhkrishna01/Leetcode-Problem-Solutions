9a64822ecdd3c5fa387aabfddcb263c16624d5fe
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-11-04 14:09:56 +0530

    Time: 19 ms (80.34%), Space: 27.9 MB (77.92%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.cpp
+++ 3163-string-compression-iii/3163-string-compression-iii.cpp
@@ -3,16 +3,19 @@ public:
     string compressedString(string word) {
         char ch = word[0];
         string s = "";
+        int count = 0;
         for(int i = 0; i < word.size(); i++){
             if(ch == word[i] && count < 9){
                 count++;
             }
             else{
+                s.push_back(count + '0');
                 s.push_back(ch);
                 ch = word[i];
                 count = 1;
             }
         }
+        s.push_back(count + '0');
         s.push_back(ch);
         return s;
     }
