5974e39b8c813ed543af81a536c1211fae4e4672
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2024-11-18 12:20:53 +0530

    Time: 0 ms (100%), Space: 10.4 MB (60.07%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size();
+        vector<int> ans(n, 0);
+        
+        if(k==0){ 
+            return ans;
+        }
+
+        if(k>0){
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % n]; 
+                }
+                ans[i] = sum;
+            }
+        }
+
+        if(k<0){
+            k = abs(k);
+            for (int i = 0; i < n; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i-j+n) % n]; 
+                }
+                ans[i] = sum;
+            }
+        }
+
+        return ans;
+    }
+};
