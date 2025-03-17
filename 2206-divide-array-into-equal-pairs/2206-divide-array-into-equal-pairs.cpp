06070514a37fe1fb9247603171b41e9d07f08f04
Krishna Singh <115335429+singhkrishna01@users.noreply.github.com>
2025-03-17 10:41:59 +0530

    Time: 4 ms (42.39%), Space: 18.1 MB (49.04%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.cpp
@@ -7,7 +7,7 @@ public:
             mp[nums[i]]++;
             
         for(int i=0;i<n;i++){
-        if(mp[nums[i]]%2 != 1)
+        if(mp[nums[i]]%2 != 0)
         return false;
         }
         return true;
