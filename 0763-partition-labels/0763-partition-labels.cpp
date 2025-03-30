42a84d2fc7fbf896e69ecc3d6fd9e05ea7bc1102
Krishna Singh <krishna00cs@gmail.com>
2025-03-30 11:55:03 +0530

    Time: 3 ms (30.82%), Space: 9.1 MB (26.99%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -11,6 +11,7 @@ public:
 
         for (int i = 0; i < s.size(); i++) {
             end = max(end, last_occurrence[s[i]]);
+            if (i == end) {
                 result.push_back(end - start + 1);
                 start = i + 1;
             }
