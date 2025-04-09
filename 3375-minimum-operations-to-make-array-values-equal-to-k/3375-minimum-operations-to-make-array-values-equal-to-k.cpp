a0c480bdb91a1043660ebce1895b7c51a6a72bb1
Krishna Singh <krishna00cs@gmail.com>
2025-04-09 17:02:41 +0530

    Time: 3 ms (99.06%), Space: 31 MB (99.69%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     static int minOperations(vector<int>& nums, int k) {
         bitset<101> hasX=0;
+        int xMin=101;
         [[unroll]]
         for(int x: nums){
             hasX[x]=1;
