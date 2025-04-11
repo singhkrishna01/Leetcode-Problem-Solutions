d750372d542693995fac036995b4aa1180b85087
Krishna Singh <krishna00cs@gmail.com>
2025-04-11 19:26:16 +0530

    Time: 68 ms (58.71%), Space: 9.8 MB (43.75%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.cpp
@@ -5,6 +5,7 @@ public:
         for (int num = low; num <= high; num++) {
             string s = to_string(num);
             int n = s.size();
+            if (n % 2 != 0) continue;
             int mid = n / 2, left = 0, right = 0;
             for (int i = 0; i < mid; i++) {
                 left += s[i] - '0';
