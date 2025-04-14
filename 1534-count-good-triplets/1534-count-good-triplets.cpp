11e03787501c796ae0dea71c12d792299ae78d19
Krishna Singh <krishna00cs@gmail.com>
2025-04-14 19:28:32 +0530

    Time: 11 ms (27%), Space: 11.1 MB (43.88%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -4,6 +4,7 @@ public:
         int increment = 0;
         for (int i = 0; i < arr.size(); ++i) {
             for (int j = i + 1; j < arr.size(); ++j) {
+                for (int k = j + 1; k < arr.size(); ++k) {
                     if (std::abs(arr[i] - arr[j]) <= a) {
                         if (std::abs(arr[j] - arr[k]) <= b) {
                             if (std::abs(arr[i] - arr[k]) <= c) {
