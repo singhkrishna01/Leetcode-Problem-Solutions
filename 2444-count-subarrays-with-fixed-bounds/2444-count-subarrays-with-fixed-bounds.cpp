3ca595244bd08f09e0dc0eabc66aea2b2b9d845f
Krishna Singh <krishna00cs@gmail.com>
2025-04-26 10:58:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -5,17 +5,20 @@ public:
         int bad_idx = -1, left_idx = -1, right_idx = -1;
 
         for (int i = 0; i < nums.size(); ++i) {
-            if (!(minK <= nums[i] && nums[i] <= maxK)) {
                 bad_idx = i;
             }
+
             if (nums[i] == minK) {
                 left_idx = i;
             }
+
             if (nums[i] == maxK) {
                 right_idx = i;
             }
+
             res += max(0, min(left_idx, right_idx) - bad_idx);
         }
+
         return res;
     }
 };
