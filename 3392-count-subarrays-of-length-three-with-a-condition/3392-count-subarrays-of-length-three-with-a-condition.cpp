16a9282e551643814cd174392fbd97aa640e1fd0
Krishna Singh <krishna00cs@gmail.com>
2025-04-27 12:15:15 +0530

    Time: 1 ms (27.58%), Space: 48.3 MB (75.3%) - LeetHub

--- 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.cpp
+++ 3392-count-subarrays-of-length-three-with-a-condition/3392-count-subarrays-of-length-three-with-a-condition.cpp
@@ -6,6 +6,7 @@ public:
 
         for (int i = 0; i < n - 2; i++) {
             int first = nums[i];
+            int middle = nums[i + 1];
             int third = nums[i + 2];
 
             if ((first + third) * 2 == middle) {
