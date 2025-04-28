a964d4599137c56821c26c398ebb7ce8a595d2ae
Krishna Singh <krishna00cs@gmail.com>
2025-04-28 10:52:49 +0530

    Time: 2 ms (66.39%), Space: 99 MB (76.24%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.cpp
@@ -5,10 +5,10 @@ public:
         for (int right = 0, left = 0; right < nums.size(); right++) {
             part_sum += nums[right];
             while (part_sum * (right - left + 1) >= k && left <= right) {
+                part_sum -= nums[left];
                 left++;
             }
             res += right - left + 1;
-            
         }
         return res;
     }
