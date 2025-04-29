64f4780b1953d1c5049b7c7e781ceff2db8b9361
Krishna Singh <krishna00cs@gmail.com>
2025-04-29 15:17:18 +0530

    Time: 11 ms (22.61%), Space: 121.7 MB (48.26%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -6,6 +6,7 @@ public:
         while(right<nums.size() || left>right){
             if(nums[right] == max_num)count++;
             while(count>=k){
+                if(nums[left]==max_num)count--;
                 left++;
                 ans += nums.size()-right;
             }    
