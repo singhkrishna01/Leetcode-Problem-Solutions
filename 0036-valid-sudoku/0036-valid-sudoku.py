f4c3c2cb8aa15ccbd28908a777dff8fac9e8ab45
Krishna Singh <krishna00cs@gmail.com>
2025-08-30 22:49:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        for row in range(9):
+            for col in range(9):
+                    if not self.isSafe(board, row, col, board[row][col]):
+                        return False
+        return True.
+
+    def isSafe(self, sudoku: list[list[str]], row: int, col: int, digit: str) -> bool:
+        for i in range(9):
+            if i != col and sudoku[row][i] == digit:
+                return False
+
+        for i in range(9):
+            if i != row and sudoku[i][col] == digit:
+                return False
+
+        subgridStartRow = (row // 3) * 3
+        subgridStartCol = (col // 3) * 3
+        for i in range(subgridStartRow, subgridStartRow + 3):
+            for j in range(subgridStartCol, subgridStartCol + 3):
+                if (i != row or j != col) and sudoku[i][j] == digit:
+                    return False
+
+        return True
