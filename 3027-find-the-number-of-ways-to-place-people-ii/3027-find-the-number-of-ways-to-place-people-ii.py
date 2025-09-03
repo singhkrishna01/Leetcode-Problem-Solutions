1caabd358e00370a44e30cef16fd6e6763e3455b
Krishna Singh <krishna00cs@gmail.com>
2025-09-03 14:12:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3027-find-the-number-of-ways-to-place-people-ii/3027-find-the-number-of-ways-to-place-people-ii.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numberOfPairs(self, points):
+        points.sort(key=lambda p:(p[0],-p[1]))
+        c=0
+        for i,(xi, yi) in enumerate(points):
+            b=float('-inf')
+            for xj, yj in points[i+1:]:
+                if yi>=yj>b:
+                    b=yj
+        return c
