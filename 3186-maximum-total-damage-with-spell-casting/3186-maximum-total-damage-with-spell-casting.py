7bfa6bc5e9b61229e3b456de4f952c74026ae61c
Krishna Singh <krishna00cs@gmail.com>
2025-10-11 17:38:58 +0530

    Time: 380 ms (91.5%), Space: 40 MB (76.25%) - LeetHub

---
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def maximumTotalDamage(self, power: list[int]) -> int:
+        cnt = Counter(power)
+        arr = sorted(cnt.keys())
+        n = len(arr)
+        dp = [0]*n
+        for i in range(n):
+            val = arr[i]*cnt[arr[i]]
+            j = i-1
+            while j>=0 and arr[i]-arr[j]<=2:
+                j-=1
+            dp[i] = max(dp[i-1] if i>0 else 0, val + (dp[j] if j>=0 else 0))
+        return dp[-1]
