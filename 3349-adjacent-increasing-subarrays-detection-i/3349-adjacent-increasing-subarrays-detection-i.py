29572b3ea49034694a67840c3c505dce9e32f318
Krishna Singh <krishna00cs@gmail.com>
2025-10-14 10:30:38 +0530

    Time: 80 ms (75.4%), Space: 17.6 MB (86.63%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.py
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.py
@@ -4,6 +4,7 @@ class Solution:
         if knew == 0:
             return True
         for j in range(k + 1, len(nums)):
+            if nums[j] > nums[j - 1] and nums[j - k] > nums[j - k - 1]:
                 knew -= 1
             else:
                 knew = k - 1
