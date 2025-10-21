91525da29a7e9422346704318a80c2ded06fc580
Krishna Singh <krishna00cs@gmail.com>
2025-10-21 12:25:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3346-maximum-frequency-of-an-element-after-performing-operations-i/3346-maximum-frequency-of-an-element-after-performing-operations-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int, numOperations: int) -> int:
+        n = max(nums)
+        count = [0] * (n + 1)
+        for num in nums:hhh
+            count[num] += 1
+        presum = [0]               
+        for i in range(n + 1):
+            presum.append(presum[-1] + count[i])
+        ans = 0
+        for i in range(n + 1):
+            left = presum[i] - presum[max(0, i-k)]         
+            cur = count[i] + min(numOperations, left + right)   
+            ans = max(ans, cur)                           
+        return ans
