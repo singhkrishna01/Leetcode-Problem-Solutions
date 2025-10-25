71a17cf8a6e1a2e37a0849da5c091ece19683cba
Krishna Singh <krishna00cs@gmail.com>
2025-10-25 19:09:08 +0530

    Time: 0 ms (100%), Space: 17.9 MB (16.22%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -3,4 +3,5 @@ class Solution:
         return (n * (n + 1)) >> 1
 
     def totalMoney(self, days: int) -> int:
+        nWeeks, rDays = divmod(days, 7)        
         return self.triSum(days) - 42 * self.triSum(nWeeks - 1) - 6 * nWeeks * rDays
